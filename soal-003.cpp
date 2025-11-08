8fa14e78334f9f68686b5f4f9981d934ef90ec90
juldmir <haifajulda@gmail.com>
2025-11-08 21:51:34 +0700

    Add files via upload

---
+++ soal-003.cpp
@@ -0,0 +1,48 @@
+/*
+Tanggal     : 08 - 11 - 2025
+Deskripsi   : Shell Sort
+Waktu       : 30 menit
+*/ 
+
+
+#include <iostream>
+using namespace std;
+
+
+void shellSort(int arr[], int n) {
+    for (int gap = n / 2; gap > 0; gap /= 2) {
+    
+        for (int i = gap; i < n; i++) {
+            int temp = arr[i];
+            int j;
+
+            for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+                arr[j] = arr[j - gap];
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+
+int main() {
+    int arr[] = {23, 12, 1, 8, 34, 54, 2, 3};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Array sebelum diurutkan: ";
+    printArray(arr, n);
+
+    shellSort(arr, n);
+
+    cout << "Array setelah diurutkan (Shell Sort): ";
+    printArray(arr, n);
+
+    return 0;
+}

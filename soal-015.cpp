2153d92ffff9003535d69c6a0d0c5fe21e5ab750
juldmir <haifajulda@gmail.com>
2025-11-21 00:16:01 +0700

    Add files via upload

---
+++ soal-015.cpp
@@ -0,0 +1,17 @@
+
+/*
+Tanggal     : 20 - 11 - 2025
+Deskripsi   : Penggunaan loop
+Waktu       : 13 menit
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    for (int i=1;i<=n;i++){
+        cout << i;
+    }
+}
